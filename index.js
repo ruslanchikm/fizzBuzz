1430c6917080e85d17718b42f81cd483375095b3
ruslanchikm <ruslanmatran@gmail.com>
2023-11-01 13:24:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+
+// // Execticiul 1
+
+// function fizzBuzz() {
+//     for (let i = 1; i <= 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('fizzBuzz');
+//         }
+//          else if (i % 3 === 0) {
+//             console.log('Fizz');
+//         }
+//         else if ( i % 5 === 0) {
+//             console.log('Buzz');
+//         }
+//         else {
+//             console.log(i);
+//         }
+  
+//     }
+// }
+
+// fizzBuzz();
+
+// Exercitiul 2
+
+function findMissingNumber(arr) {
+    if (!Array.isArray(arr)) {
+      return "Argumentul dat nu este un array.";
+    }
+  
+    const n = arr.length + 1; // Numărul total de elemente, inclusiv numărul lipsă
+    const expectedSum = (n * (n + 1)) / 2; // Suma elementelor dintr-un array complet
+  
+    const actualSum = arr.reduce((acc, current) => acc + current, 0);
+  
+    const missingNumber = expectedSum - actualSum;
+  
+    return missingNumber;
+  }
+  
+  // Exemplu de utilizare:
+  const cifre = [1, 2, 3, 5]; // Lipsește cifra 4
+  const numarLipsa = findMissingNumber(cifre);
+  console.log(`Numărul lipsă este: ${numarLipsa}`);
+  
